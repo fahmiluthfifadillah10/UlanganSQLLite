61c71340a6bb4fb4016f81aa66df1c92b59cd262
Fahmi Luthfi Fadillah <fahmiluthfi10.siswa@smkn4bdg.sch.id>
2021-02-24 16:05:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ulangansqllite/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ulangansqllite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000L); // 3000 L = 3 DETIK
+    }
+}
